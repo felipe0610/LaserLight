c2538aea63f20f42ff84851e07b40fdbcba3c152
felipe0610 <fcalderoncas@gmail.com>
2015-08-18 03:23:25 -0600

    Fondo Cuadriculado

--- core/src/com/LaserLight/fccs/Game.java
+++ core/src/com/LaserLight/fccs/Game.java
@@ -2,47 +2,96 @@ package com.LaserLight.fccs;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-public class Game extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Texture img;
+public class Game extends ApplicationAdapter implements InputProcessor {
+	Texture img;
+	TiledMap tiledMap;
 	OrthographicCamera camera;
-	Sprite sprite;
+	TiledMapRenderer tiledMapRenderer;
 
-	
+	//sdfsdf
 	@Override
 	public void create () {
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
-		camera = new OrthographicCamera(1,h/w);
-		batch = new SpriteBatch();
-		img= new Texture("fondoProyecto.jpg");
-		img.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
-		TextureRegion region = new TextureRegion(img,0,0,1920,1200);
-		sprite = new Sprite(region);
-		sprite.setSize(1f,
-				1f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth() / 2,
-				sprite.getHeight() / 2);
-		sprite.setPosition(-sprite.getWidth() / 2,
-				-sprite.getHeight() / 2);
+
+		//camera = new OrthographicCamera(w,h);
+		//camera.setToOrtho(false, w/2, h/2);
+		//camera.position.set(500, 500, 0);
+		//camera.update();
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false,w/1.08f,h/1.08f);
+		camera.position.set(500, 500, 0); //by default camera position on (0,0,0)
+		camera.update();
+
+		tiledMap = new TmxMapLoader().load("fondoCuadriculado.tmx");
+		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+		Gdx.input.setInputProcessor(this);
 
 
 	}
+
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(0, 0, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
+		camera.update();
+		tiledMapRenderer.setView(camera);
+		tiledMapRenderer.render();
 	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+
+		return false;
+	}
+
+
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
+	}
+
 }
